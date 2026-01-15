aefd5f4b0bf9fedf752a56f69dca07dca5e4b1cf
GoWuGo <jindwu11@gmail.com>
2026-01-15 17:59:49 +0900

    Leadership project

---
+++ assets/javascript/initData.js
@@ -0,0 +1,216 @@
+const resolutionInfo = {
+    "web": [
+      {
+        "label": "网页 1920",
+        "value": "1920x1080",
+        "w": 1920,
+        "h": 1080
+      },
+      {
+        "label": "网页 1440",
+        "value": "1440x1024",
+        "w": 1440,
+        "h": 1024
+      },
+      {
+        "label": "MacBook Pro 16\"",
+        "value": "1728x1117",
+        "w": 1728,
+        "h": 1117
+      },
+      {
+        "label": "MacBook Pro 14\"",
+        "value": "1512x982",
+        "w": 1512,
+        "h": 982
+      },
+      {
+        "label": "MacBook Pro",
+        "value": "1440x900",
+        "w": 1440,
+        "h": 900
+      },
+      {
+        "label": "MacBook Air 13",
+        "value": "1280x832",
+        "w": 1280,
+        "h": 832
+      },
+      {
+        "label": "iMac",
+        "value": "2240x1260",
+        "w": 2240,
+        "h": 1260
+      },
+      {
+        "label": "华为 MateBook 16s",
+        "value": "1260x840",
+        "w": 1260,
+        "h": 840
+      },
+      {
+        "label": "华为 MateBook 14s",
+        "value": "1260x840-14",
+        "w": 1260,
+        "h": 840
+      }
+    ],
+    "app": [
+      {
+        "label": "iPhone 16 Pro Max",
+        "value": "440x956-16pm",
+        "w": 440,
+        "h": 956
+      },
+      {
+        "label": "iPhone 16 Pro",
+        "value": "402x874-16p",
+        "w": 402,
+        "h": 874
+      },
+      {
+        "label": "iPhone 16 Plus",
+        "value": "430x932-16+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 16",
+        "value": "393x852-16",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Pro Max",
+        "value": "430x932-15pm",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15 Pro",
+        "value": "393x852-15p",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Plus",
+        "value": "430x932-15+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15",
+        "value": "393x852-15",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 14 Plus",
+        "value": "428x926-14+",
+        "w": 428,
+        "h": 926
+      },
+      {
+        "label": "iPhone 14",
+        "value": "390x844-14",
+        "w": 390,
+        "h": 844
+      },
+      {
+        "label": "iPhone 13 mini",
+        "value": "375x812-13m",
+        "w": 375,
+        "h": 812
+      },
+      {
+        "label": "iPhone 11",
+        "value": "414x896-11",
+        "w": 414,
+        "h": 896
+      },
+      {
+        "label": "iPhone SE",
+        "value": "320x568-se",
+        "w": 320,
+        "h": 568
+      },
+      {
+        "label": "Mate 70",
+        "value": "405x896-m70",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 70 Pro",
+        "value": "438x944-m70p",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 70 Pro+",
+        "value": "438x944-m70p+",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 60",
+        "value": "405x896-m60",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 60 Pro",
+        "value": "420x907-m60p",
+        "w": 420,
+        "h": 907
+      },
+      {
+        "label": "Mate XT-单屏",
+        "value": "336x744-mxt-s",
+        "w": 336,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-双屏",
+        "value": "682x744-mxt-d",
+        "w": 682,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-三屏",
+        "value": "1061x744-mxt-t",
+        "w": 1061,
+        "h": 744
+      },
+      {
+        "label": "Mate X5-展开",
+        "value": "741x832-mx5-u",
+        "w": 741,
+        "h": 832
+      },
+      {
+        "label": "Mate X5-折叠",
+        "value": "360x835-mx5-f",
+        "w": 360,
+        "h": 835
+      },
+      {
+        "label": "小米 15",
+        "value": "400x890-mi15",
+        "w": 400,
+        "h": 890
+      },
+      {
+        "label": "Samsung Galaxy S23",
+        "value": "360x7-s23",
+        "w": 360,
+        "h": 780
+      }
+    ]
+  }
+  
+const treeArr = [{"appuuid":"1991101715176226816","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-19 19:10:16","createUser":"WX20251023BA2AWJ","default":false,"folderFlag":false,"id":431451,"name":"ch","pageName":"ch","pageScene":"web","pageType":"0","pageUuid":"1981241502117920768","remarks":"","sort":2,"status":"0","updateTime":"2025-11-19 20:35:30","updateUser":"WX20251023BA2AWJ","url":"./assets/page/1981241502117920768.html","uuid":"1981241502117920768"},{"appuuid":"1991101715176226816","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-19 20:34:48","createUser":"WX20251023BA2AWJ","default":false,"folderFlag":false,"id":431700,"name":"jap","pageName":"jap","pageScene":"web","pageType":"0","pageUuid":"1991122987364909056","remarks":"","sort":2,"status":"0","updateTime":"2025-11-19 20:35:15","updateUser":"WX20251023BA2AWJ","url":"./assets/page/1991122987364909056.html","uuid":"1991122987364909056"},{"appuuid":"1991101715176226816","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-19 19:49:33","createUser":"WX20251023BA2AWJ","default":false,"folderFlag":false,"id":431559,"name":"laji","pageName":"laji","pageScene":"web","pageType":"0","pageUuid":"1991111600546775040","sort":7,"status":"0","updateTime":"2025-11-19 20:15:24","updateUser":"WX20251023BA2AWJ","url":"./assets/page/1991111600546775040.html","uuid":"1991111600546775040"},{"appuuid":"1991101715176226816","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-19 20:35:43","createUser":"WX20251023BA2AWJ","default":false,"folderFlag":false,"id":431702,"name":"laji jap","pageName":"laji jap","pageScene":"web","pageType":"0","pageUuid":"1991123219012124672","sort":7,"status":"0","updateTime":"2025-11-19 20:35:56","updateUser":"WX20251023BA2AWJ","url":"./assets/page/1991123219012124672.html","uuid":"1991123219012124672"}]
+
+const projectName = '领导力'
+
+const appDoc = {"1981241502117920768":"\n"}
